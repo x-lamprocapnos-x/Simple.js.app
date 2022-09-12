205d0fb177f7c69bac485660419152b829627f27
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2022-09-12 16:25:03 -0400

    updated script.js
    wrapped pokemonList in iife
    added furret to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,46 @@
-let pokemonList = 
-[ //create an array of pokemon objects
-    //remember arrays start at 0
-    {
-    name: 'Charmander',
-    height: 0.6,
-    type: ['fire']
-    },
-    {
-    name: 'Cubone',
-    height: 0.4,
-    type: ['ground']
-    },
-    {
-    name: 'Psyduck',
-    height: 0.8,
-    type: ['water']
-    },
-    {
-    name: 'Sandshrew',
-    height: 0.6,
-    type: ['ground']
-    },
-    {
-    name: 'Celebi',
-    height: 0.6,
-    type: ['psychic', 'grass']
-    }
-];
-
+let pokemonRepository = (function () {
+    let pokemonList = 
+    [ //array of pokemon
+        {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire']
+        },
+        {
+        name: 'Cubone',
+        height: 0.4,
+        type: ['ground']
+        },
+        {
+        name: 'Psyduck',
+        height: 0.8,
+        type: ['water']
+        },
+        {
+        name: 'Sandshrew',
+        height: 0.6,
+        type: ['ground']
+        },
+        {
+        name: 'Celebi',
+        height: 0.6,
+        type: ['psychic', 'grass']
+        }
+    ];
+//forEach fuction allowing pokemon list to be displayed
 pokemonList.forEach(function(pokemon) {
     document.write(pokemon.name + '<br>' + pokemon.height + '<br>' + pokemon.type + '<br>')
 });
+  
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
+  console.log(pokemonRepository.getAll()); //get pokemonList array
+pokemonRepository.add({ name: 'Furret', height: 1.8, type:'normal' }); //add pokemon 'furret"
+console.log(pokemonRepository.getAll()); //get pokemonList array
